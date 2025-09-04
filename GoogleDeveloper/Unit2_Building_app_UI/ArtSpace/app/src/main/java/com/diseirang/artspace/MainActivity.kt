2c5d90959031d0793feb9a6498854f93079d221c
Diseirang <diseirang.chheun@mjqeducation.edu.kh>
2025-09-04 17:15:49 +0700

    Update UI ArtSpace app

--- GoogleDeveloper/Unit2_Building_app_UI/ArtSpace/app/src/main/java/com/diseirang/artspace/MainActivity.kt
+++ GoogleDeveloper/Unit2_Building_app_UI/ArtSpace/app/src/main/java/com/diseirang/artspace/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.diseirang.artspace
 
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -12,13 +13,16 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
@@ -26,10 +30,18 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -46,7 +58,7 @@ class MainActivity : ComponentActivity() {
                 Surface(
                     Modifier.fillMaxSize()
                 ) {
-
+                    ArtSpaceScreen()
                 }
             }
         }
@@ -65,49 +77,144 @@ fun ArtSpaceScreen(modifier: Modifier = Modifier) {
     ) {
         val padding = 20.dp
 
-        ImageCard(
-            imageResourceId = R.drawable.image_1,
-        )
-        Spacer(Modifier.size(padding))
+        var currentImageIndex by remember { mutableIntStateOf(0) }
 
-            // Artwork Image Placeholder
-            Box(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .background(Color.LightGray),
-                contentAlignment = Alignment.Center
-            ) {
+        var currentImage = R.drawable.image_1
+        var currentTitle = R.string.title_1
+        var currentDescription = R.string.description_1
 
-                Column(
-                    modifier = Modifier
-                    .padding(top = padding, bottom = padding),
-                    horizontalAlignment = Alignment.Start
-                ) {
-                    Text(
-                        text = stringResource(R.string.title_1),
-                        style = MaterialTheme.typography.headlineSmall,
-                        fontWeight = FontWeight.Bold
-                    )
-                    Text(
-                        text = stringResource(R.string.description_1),
-                        style = MaterialTheme.typography.headlineSmall,
-                        fontWeight = FontWeight.Bold
-                    )
-                }
+        when (currentImageIndex) {
+            0 -> {
+                currentImage = R.drawable.image_1
+                currentTitle = R.string.title_1
+                currentDescription = R.string.description_1
             }
 
+            1 -> {
+                currentImage = R.drawable.image_2
+                currentTitle = R.string.title_2
+                currentDescription = R.string.description_2
+            }
+
+            2 -> {
+                currentImage = R.drawable.image_3
+                currentTitle = R.string.title_3
+                currentDescription = R.string.description_3
+            }
+
+            3 -> {
+                currentImage = R.drawable.image_4
+                currentTitle = R.string.title_4
+                currentDescription = R.string.description_4
+            }
+
+            4 -> {
+                currentImage = R.drawable.image_5
+                currentTitle = R.string.title_5
+                currentDescription = R.string.description_5
+            }
+
+            5 -> {
+                currentImage = R.drawable.image_6
+                currentTitle = R.string.title_6
+                currentDescription = R.string.description_6
+            }
+        }
+
+        ShadowedImageBox(imageRes = currentImage)
+        Spacer(Modifier.size(padding))
+        // Artwork Image Placeholder
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color.LightGray),
+            contentAlignment = Alignment.Center
+        ) {
+            Column(
+                modifier = Modifier
+                    .padding(top = padding, bottom = padding),
+                horizontalAlignment = Alignment.Start
+            ) {
+                Text(
+                    text = stringResource(currentTitle),
+                    style = MaterialTheme.typography.headlineSmall,
+                    fontWeight = FontWeight.Bold
+                )
+                Text(
+                    text = stringResource(currentDescription),
+                    style = MaterialTheme.typography.headlineSmall,
+                    fontWeight = FontWeight.Bold
+                )
+            }
+        }
+        Spacer(Modifier.size(padding))
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            CommonButton(
+                onClick = {
+                    if (currentImageIndex > 0) {
+                        currentImageIndex--
+                    }
+                    else{
+                        currentImageIndex =5
+                    }
+                },
+                buttonText = "Previous"
+            )
+            CommonButton(
+                onClick = {
+                    if (currentImageIndex <= 4) {
+                        currentImageIndex++
+                    }
+                    else{
+                        currentImageIndex =0
+                    }
+
+                },
+                buttonText = "Next"
+            )
+        }
     }
 }
 
 @Composable
-fun ImageCard(
-    imageResourceId: Int,
-) {
-        Image(
-            painter = painterResource(imageResourceId),
-            contentDescription = null
-        )
+fun CommonButton(
 
+    onClick: () -> Unit,
+    buttonText: String
+) {
+    androidx.compose.material3.Button(
+        onClick = onClick, modifier = Modifier.width(150.dp)
+    ) {
+        Text(buttonText)
+    }
+}
+
+@Composable
+fun ShadowedImageBox(imageRes: Int) {
+    Box(
+        modifier = Modifier
+            .padding(10.dp) // Padding from the screen edge
+            .shadow(
+                elevation = 8.dp,
+                shape = RoundedCornerShape(15.dp),
+                clip = false
+            )
+            .background(Color.White, shape = RoundedCornerShape(12.dp))
+            .padding(12.dp) // Inner padding inside the box
+    ) {
+        Image(
+            painter = painterResource(id = imageRes),
+            contentDescription = "Artwork",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(500.dp)
+                .clip(RoundedCornerShape(9.dp))
+        )
+    }
 }
 
 @Preview(showBackground = true)
